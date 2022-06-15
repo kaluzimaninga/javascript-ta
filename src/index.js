3073bfc09dc8c5a39af6f3ef3857a2c88ade1913
kaluzimaninga <maningamarshall@gmail.com>
2022-06-15 23:08:57 +0200

    added solution to code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // complete the function
 function prime (num) {
   // code goes here
+  for (var i = 2; i < num; i++)
+    if(num % i === 0) return false;
+  return num > 1;
 }
 
 function solution (arg) {
